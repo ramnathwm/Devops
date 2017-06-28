0440f56d6c84b8e70fa3c84f9dc25cc805581957
ramece <ramnathreddyece@gmail.com>
2017-06-28 18:26:49 +0530

    commiting jenkins related and maven related files

---
+++ Jenkins/Calculator.java
@@ -0,0 +1,8 @@
+class Calculator {
+  public int evaluate(String expression) {
+    int sum = 0;
+    for (String summand: expression.split("\\+"))
+      sum += Integer.valueOf(summand);
+    return sum;
+  }
+}
